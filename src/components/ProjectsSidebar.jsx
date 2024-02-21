8c8c1981a224e56619d5e49affb3533b9818537a
wangkodok <wangkodok@gmail.com>
2024-02-21 19:08:50 +0900

    프로젝트 사이드바 컴포넌트 생성

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,13 @@
+export default function ProjectsSidebar() {
+  return (
+    <aside>
+      <h2>프로젝트</h2>
+      <div>
+        <button>추가</button>
+      </div>
+      <ul>
+        <li></li>
+      </ul>
+    </aside>
+  );
+}
