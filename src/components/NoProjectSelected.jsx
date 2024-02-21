42538b5c85b583bb2d93517e4ce0679efd8f6538
wangkodok <wangkodok@gmail.com>
2024-02-21 20:51:52 +0900

    JSX와 테일윈드 스타일을 위한 컴포넌트 분리 (재사용 가능성 향상)

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,23 @@
+import noProjectImage from "../assets/no-projects.png";
+import Button from "./Button";
+
+export default function NoProjectSelected() {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        className="w-16 h-16 object-contain mx-auto"
+        src={noProjectImage}
+        alt="빈 할 일 목록"
+      />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        선택된 프로젝트가 없습니다.
+      </h2>
+      <p className="text-stone-400 mb-4">
+        프로젝트를 선택하거나 새로운 프로젝트를 시작하십시오.
+      </p>
+      <p className="mt-8">
+        <Button>새 프로젝트 추가</Button>
+      </p>
+    </div>
+  );
+}
