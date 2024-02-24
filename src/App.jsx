1fc82e2e2a65583052d1cdba5abb4be70eb60775
wangkodok <wangkodok@gmail.com>
2024-02-24 23:26:44 +0900

    새 프로젝트를 추가하고 저장 시 목록 생성

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,14 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
@@ -43,7 +45,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
       {content}
     </main>
   );
